172546a1ee84238c1ee5984ecb315eb6fc592a98
hexaspace <hexa6do@naver.com>
2023-05-31 18:15:41 +0900

    Feat: click counter use state

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,21 +1,52 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+  void onClick() {
+    setState(() {
+      counter = counter + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('hello flutter'),
+          title: const Text('stateful widgets'),
           centerTitle: true,
           elevation: 99,
         ),
         body: Center(
-          child: Text("hello world"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Counter',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(fontSize: 30),
+              ),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClick,
+                icon: const Icon(Icons.add_box_outlined),
+              ),
+            ],
+          ),
         ),
       ),
     );

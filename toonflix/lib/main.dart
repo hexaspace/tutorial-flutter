35ef787559cd7f5a32a7bc180791a93cf4f6bcf9
hexaspace <hexa6do@naver.com>
2023-05-31 20:36:56 +0900

    Feat: flex interface

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
-  runApp(App());
+  // pomo 어플
+  // 25분 일하고 5분 쉬기
+  // https://www.behance.net/gallery/98918603/POMO-UIKIT?tracking_source=search_projects%7Cpomo+uikit
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('hello flutter'),
-          centerTitle: true,
-          elevation: 99,
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
         ),
-        body: Center(
-          child: Text("hello world"),
+        cardColor: const Color(0xFFF4EDDB),
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
         ),
       ),
+      home: const HomeScreen(),
     );
   }
 }

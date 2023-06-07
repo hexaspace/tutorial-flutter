11f966321a012dbe49350055f373b62b50c7b54e
hexaspace <hexa6do@naver.com>
2023-06-07 19:23:10 +0900

    Feat: detail text data

--- toonflix/lib/screens/detail_screen.dart
+++ toonflix/lib/screens/detail_screen.dart
@@ -74,6 +74,42 @@ class _DetailScreenState extends State<DetailScreen> {
             ),
           ],
         ),
+        const SizedBox(
+          height: 24,
+        ),
+        FutureBuilder(
+          future: webtoon,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 50,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      snapshot.data!.about,
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "${snapshot.data!.genre} / ${snapshot.data!.age}",
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }
+            return const Text("...");
+          },
+        ),
       ]),
     );
   }

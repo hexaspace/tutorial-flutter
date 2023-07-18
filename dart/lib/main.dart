7df1551a3715aa69a96c35fdc8268e092a068353
hexaspace <hexa6do@naver.com>
2023-07-18 19:22:35 +0900

    Study: null 처리 연산

--- dart/lib/main.dart
+++ dart/lib/main.dart
@@ -5,4 +5,10 @@ void main() {
   // a = 'string';  // error
   dynamic b = 20;
   b = 'string'; // not error
+
+  // null 처리 연산자
+  int? num1; // null able
+  int num2 = 10;
+  print(num1! + num2); // error, not null이라고 !가 안심해줬지만, 실행시키면 오류 발생됨
+  print(num1 ?? 5 + num2); // success, num1이 null이면 5 + num2 실행
 }

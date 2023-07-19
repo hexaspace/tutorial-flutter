76e832a3959ed8d32b22c7e10a53f7d11f8e0208
hexaspace <hexa6do@naver.com>
2023-07-19 19:01:44 +0900

    Init: layout 시작 전 init

--- flutter_basic/lib/main.dart
+++ flutter_basic/lib/main.dart
@@ -6,7 +6,7 @@ void main() {
     // 화면 구성 도화지
     home: Scaffold(
       appBar: AppBar(
-        title: Text('Container study'),
+        title: Text(''),
       ),
       body: CustomContainer(),
     ),
@@ -18,37 +18,6 @@ class CustomContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 100,
-      // color: Color.fromARGB(255, 164, 255, 237), boxdecoration 을 사용하려면 여기서  null이어야함
-      padding: EdgeInsets.fromLTRB(10, 12, 13, 14),
-      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-      decoration: BoxDecoration(
-          color: Colors.pink.shade100,
-          border: Border.all(
-            color: Colors.pink.shade300,
-            width: 5,
-            style: BorderStyle.solid,
-          ),
-          borderRadius: BorderRadius.circular(16),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey,
-              offset: Offset(-10, 10),
-              blurRadius: 10,
-              spreadRadius: 2,
-            ),
-            BoxShadow(
-              color: Colors.blue,
-              offset: Offset(10, -10),
-              blurRadius: 10,
-              spreadRadius: 2,
-            ),
-          ]),
-      child: Center(
-        child: Text('hello containercontainercontainercontainercontainercontainercontainer'),
-      ),
-    );
+    return Container();
   }
 }
